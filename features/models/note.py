4698a9014acc98e60ed0eaf4002473cbac0a00c5
Joan <j.pastor1591@gmail.com>
2024-12-18 18:22:03 +0100

    Testing Note Model

--- features/models/note.py
+++ features/models/note.py
@@ -5,8 +5,7 @@ from sqlalchemy import ForeignKey
 
 from data.db_orm import Base
 
-# from features.models import user as us
-
+from features.models.user import User
 # from features.encrypter import encrypt_data
 
 from shared.public_id import PublicID
@@ -20,32 +19,32 @@ class Note(Base):
     """
 
     # Table settings
-    __tablename__ = "note"
+    __tablename__: str = "note"
 
     # Column settings
     id: Mapped[str] = mapped_column(primary_key=True)
     user_id: Mapped[str] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), nullable=False, index=True)
     title: Mapped[str | None]
-    content_encrypted: Mapped[str]
+    encrypted_content: Mapped[str]
     created: Mapped[date]
 
     # Relationship settings
-    # user: Mapped["us.User"] = relationship("User", back_populates="notes")
+    user: Mapped["User"] = relationship("User", back_populates="notes")
 
     # Initializer
-    def __init__(self, title: str | None, content: str, user) -> None:
+    def __init__(self, content: str, user: User, title: str | None = None) -> None:
         super().__init__()
 
         self.id: str = PublicID.generate_short_id()
         self.title: str | None = title
-        self.content_encrypted = content  # encrypt_data(content)
-        self.user = user
+        self.encrypted_content = content  # encrypt_data(content)
+        self.user: User = user
         self.created: date = date.today()
 
         # Logs new note
-        logger.debug("new note created...")
+        logger.info("Note instance created...")
 
     def __str__(self) -> str:
         return (f"<classNote: id='{self.id}', title='{self.title}', "
-                f"content_encrypted='{self.content_encrypted}', user='{self.user}', "
+                f"content_encrypted='{...}', user='{...}', "
                 f"created='{self.created.strftime("%d/%m/%Y")}'>")
--- tests/note_test.py
+++ tests/note_test.py
@@ -1,11 +1,26 @@
+import unittest
+
+from features.models.user import User
 from features.models.note import Note
-from shared.logger_setup import test_logger
+
+from shared.logger_setup import test_logger as logger
 
 
-def main():
-    new_note = Note("Test01", "Test_example", "user")
-    test_logger.info(new_note)
+class TestNote(unittest.TestCase):
+    def test_create_instance(self) -> None:
+        logger.info("Starting test note...")
 
+        user_test = User("admin", "admin@admin.com", "admin1234")
 
-if __name__ == '__main__':
-    main()
+        # Create new instances
+        note_01 = Note("TestContent", user_test)
+        note_02 = Note("TestContent", user_test, "Test02")
+        self.assertEqual(note_01.title, None, "Should be None")
+        self.assertEqual(note_02.title, "Test02", "Should be Test02")
+
+        # Register instance
+        logger.debug(note_01)
+        logger.debug(note_02)
+
+        # Finish test
+        logger.info("Ending test note...")
