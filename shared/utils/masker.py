90e7449663f219fb79a2779b2ec35b976482eb27
Joan <j.pastor1591@gmail.com>
2025-01-05 09:04:09 +0100

    Adding functions to mask data

---
+++ shared/utils/masker.py
@@ -0,0 +1,24 @@
+
+
+def mask_email(email: str) -> str:
+    local, domain = email.split("@")
+    masked = "*" * len(local)
+    return masked + "@" + domain
+
+
+def mask_username(username: str) -> str:
+    pass
+
+
+def mask_password(passsword: str) -> str:
+    return "*" * len(passsword)
+
+
+def mask_number(number: str) -> str:
+    masked = "*" * len(number[:-4])
+    return masked + number[-4:]
+
+
+def mask_phone(phone: str) -> str:
+    masked = "*" * len(phone[:-3])
+    return masked + phone[-3:]
