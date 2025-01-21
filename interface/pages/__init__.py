eabd53923190e89038b540315d8ca91a307f1974
Joan <j.pastor1591@gmail.com>
2025-01-21 20:48:36 +0100

    Created a load page class

--- interface/pages/__init__.py
+++ interface/pages/__init__.py
@@ -1,6 +1,7 @@
+from .load_page import LoadPage
 from .admin import Admin
 from .login import Login
 from .signup import Signup
 from .home import Home
 
-__all__ = ["Admin", "Login", "Signup", "Home"]
+__all__ = ["Admin", "Login", "Signup", "Home", "LoadPage"]
---
+++ interface/pages/load_page.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+from shared.utils.colors import *
+
+
+class LoadPage(ft.Container):
+
+    def __init__(self):
+        super().__init__()
+
+        # Main container settings
+        self.alignment = ft.alignment.center
+        self.expand = True
+        self.bgcolor = ft.Colors.with_opacity(0.3, lightColorBackground)
+
+        # Content
+        self.content = ft.ProgressRing(color=accentGeneralElementColor)
