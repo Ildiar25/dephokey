28f623ce9d80980a60df2c5332663ca669acd062
Joan <j.pastor1591@gmail.com>
2025-01-26 19:07:11 +0100

    Refactoring LOGIN page

--- interface/pages/login.py
+++ interface/pages/login.py
@@ -11,6 +11,7 @@ from interface.controls import *
 
 from shared.validate import Validate
 from shared.logger_setup import main_logger as logger
+from shared.utils.masker import mask_email, mask_password
 from shared.utils.colors import *
 
 
@@ -18,149 +19,181 @@ class Login(ft.Container):
     def __init__(self, page: ft.Page) -> None:
         super().__init__()
 
-        # General attributes
+        # General attributes (like info elements)
         self.page = page
         self.snackbar = ft.SnackBar(
-            bgcolor=bgSnackBarDanger,
+            bgcolor=bgSnackbarDangerColor,
             content=ft.Text(
                 "",
                 text_align=ft.TextAlign.CENTER,
-                color=mainDangerTextColor
+                color=dangerTextColor
             )
         )
 
-        # Page design
+        # Main app settings
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
         self.page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        self.page.update()
 
-        # Login attributes
+        # Login elements
         self.email = CustomTextField(
-            label="Correo electrónico",
-            autofocus=True,
-            on_change=self.enable_button
+            label="Correo Electrónico",
+            on_change=self.toggle_login_button_state
         )
         self.password = CustomTextField(
             label="Contraseña",
-            can_reveal_password=True,
+            on_change=self.toggle_login_button_state,
             password=True,
-            on_change=self.enable_button
+            can_reveal_password=True
         )
         self.login_button = CustomElevatedButton(
-            "LOGIN",
-            width=300,
-            disabled=True,
-            on_click=self.login_function
-        )
-        self.forgot_password = ft.Container(
-            on_hover=self.focus_link,
-            on_click=self.forgot_password,
-            content=ft.Text(
-                "Olvidé la contraseña",
-                color=mainSecondaryTextColor
-            )
+            "Login", bg_color=bgEButtonColor, foreground_color=tertiaryTextColor,
+            border_size=-1, expand=True, disabled=True, on_click=self.login
         )
 
-        # Main container settings
-        self.width = 800
-        self.height = 500
-        self.border_radius = 15
-        self.shadow = ft.BoxShadow(2, blur_radius=8, color=shadowLogForm)
+        # Page design
+        self.width = 1080
+        self.height = 720
+        self.border_radius = 10
 
-        # Login elements
+        # Body content
         self.content = ft.Row(
-            alignment=ft.MainAxisAlignment.CENTER,
             spacing=0,
             controls=[
                 ft.Container(
-                    width=400,
-                    height=500,
-                    alignment=ft.alignment.center,
-                    bgcolor=lightColorBackground,
-                    content=ft.Column(
-                        alignment=ft.MainAxisAlignment.CENTER,
-                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    expand=True,
+                    image=ft.DecorationImage("interface/assets/left-bgimage.png", fit=ft.ImageFit.COVER),
+                    content=ft.Stack(
+                        alignment=ft.alignment.bottom_left,
                         controls=[
                             ft.Container(
-                                width=300,
-                                height=250,
-                                bgcolor=lightColorBackground,
-                                content=ft.Column(
-                                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                                    controls=[
-                                        ft.Text("¡Bienvenido a Dephokey!", size=24),
-                                        self.email,
-                                        self.password,
-                                        self.login_button,
-                                        ft.Row(
-                                            controls=[
-                                                self.forgot_password
-                                            ]
-                                        ),
-                                        ft.Divider(color=accentElementForm, thickness=2)
-                                    ]
-                                )
+                                width=152,
+                                height=48,
+                                margin=ft.margin.only(50, 0, 0, 25),
+                                bgcolor=ft.Colors.WHITE
                             ),
-                            ft.Text("¿Aún no tienes una cuenta?"),
-                            CustomTextButton("Crea una cuenta", on_click=lambda _: self.page.go("/signup")),
-                            self.snackbar
+                            ft.Column(
+                                expand=True,
+                                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                controls=[
+                                    ft.Container(
+                                        expand=True,
+                                        alignment=ft.alignment.center,
+                                        gradient=ft.LinearGradient(bgGradientColor,
+                                                                   begin=ft.alignment.top_center,
+                                                                   end=ft.alignment.bottom_center),
+                                        content=ft.Icon(ft.Icons.LOCK_ROUNDED, size=250, color=iconAccentFormColor)
+                                    )
+                                ]
+                            )
                         ]
                     )
                 ),
                 ft.Container(
-                    width=400,
-                    height=500,
-                    alignment=ft.alignment.center,
-                    bgcolor=bgAccentForm,
-                    image=ft.DecorationImage("interface/assets/bg-image-01.png", fit=ft.ImageFit.COVER),
-                    content=ft.Icon(ft.Icons.LOCK, size=150, color=accentElementForm)
+                    expand=True,
+                    bgcolor=bgFormColor,
+                    content=ft.Column(
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        spacing=24,
+                        controls=[
+                            ft.Container(
+                                width=380,
+                                height=296,
+                                content=ft.Column(
+                                    spacing=24,
+                                    controls=[
+                                        ft.Text(
+                                            "Inicia Sesión en Dephokey",
+                                            font_family="AlbertSansB",
+                                            color=accentTextColor,
+                                            size=24
+                                        ),
+                                        ft.Column(
+                                            controls=[
+                                                self.email,
+                                                self.password,
+                                                ft.Container(
+                                                    on_hover=self.focus_link,
+                                                    on_click=lambda _: print("/forgot_password"),
+                                                    content=ft.Text(
+                                                        "¿Has olvidado la contraseña?",
+                                                        color=accentTextColor
+                                                    )
+                                                )
+                                            ]
+                                        ),
+                                        ft.Row(
+                                            controls=[
+                                                self.login_button
+                                            ]
+                                        ),
+                                        self.snackbar
+                                    ]
+                                )
+                            ),
+                            ft.Container(
+                                width=380,
+                                content=ft.Row(
+                                    controls=[
+                                        ft.Text("No tienes cuenta?"),
+                                        ft.Container(
+                                            on_hover=self.focus_link,
+                                            on_click=lambda _: self.page.go("/signup"),
+                                            content=ft.Text(
+                                                "Regístrate en Dephokey!",
+                                                color=accentTextColor
+                                            )
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    )
                 )
             ]
         )
 
-    def enable_button(self, _: ft.ControlEvent) -> None:
-        if self.email.value and self.password.value:
+    logger.info("Creación de la página 'LOGIN' realizada.")
+
+    @staticmethod
+    def focus_link(cursor: ft.ControlEvent) -> None:
+        if cursor and cursor.control.content.color == accentTextColor:
+            cursor.control.content.color = secondaryTextColor
+            cursor.control.content.style = ft.TextStyle(
+                decoration=ft.TextDecoration.UNDERLINE,
+                decoration_color=secondaryTextColor
+            )
+        else:
+            cursor.control.content.color = accentTextColor
+            cursor.control.content.style = None
+
+        cursor.control.update()
+
+    def toggle_login_button_state(self, _: ft.ControlEvent) -> None:
+        if all((self.email.value, self.password.value)):
             self.login_button.disabled = False
         else:
             self.login_button.disabled = True
-
         self.login_button.update()
 
-    def focus_link(self, action: ft.ControlEvent):
-        if action and self.forgot_password.content.color == mainSecondaryTextColor:
-            self.forgot_password.content.color = accentElementForm
-        else:
-            self.forgot_password.content.color = mainSecondaryTextColor
+    def login(self, _: ft.ControlEvent) -> None:
 
-        self.forgot_password.update()
-
-    def forgot_password(self, _: ft.ControlEvent):
-        try:
-            raise NotImplementedError("Implementar lógica nueva contraseña vía email")
-
-        except NotImplementedError as error_message:
-            logger.error(f"Llamada a la función '{Login.forgot_password.__name__}': {error_message}")
-
-    def login_function(self, _: ft.ControlEvent) -> None:
-
-        email_input: str = self.email.value.strip().lower()
-        password_input: str = self.password.value.strip()
+        email_input = self.email.value.lower().strip()
+        password_input = self.password.value.strip()
 
         # First, validate email and password
-        if not (Validate.is_valid_email(email_input) and Validate.is_valid_password(password_input)):
-            self.snackbar.content.value = ("El correo electrónico o la contraseña no son válidos.\n"
-                                           "La contraseña debe tener al menos un número, una minúscula y "
-                                           "una mayúscula.")
+        if not all((Validate.is_valid_email(email_input), Validate.is_valid_password(password_input))):
+            self.snackbar.content.value = ("El correo o la contraseña no son válidos.\n"
+                                           "La contraseña debe tener al menos un número, una mayúscula y "
+                                           "una minúscula")
             self.snackbar.open = True
             self.snackbar.update()
-
         else:
             # Second, check if email already exists
             if not session.query(User).filter(User.email == email_input).first():
                 self.snackbar.content.value = "¡El usuario no existe!"
                 self.snackbar.open = True
                 self.snackbar.update()
-
             else:
                 # Third, load user and hashed input password
                 user: User = session.query(User).filter(User.email == email_input).first()
@@ -168,6 +201,8 @@ class Login(ft.Container):
 
                 # Compare data inputs with loaded data
                 if not all((user.email == email_input, user.hashed_password == hashed_password)):
+                    logger.warning("Inicio de sesión fallido...")
+                    logger.debug(f" >>> Datos: '{mask_email(email_input)}' - '{mask_password(password_input)}'")
                     self.snackbar.content.value = "El correo electrónico o la contraseña no son válidos."
                     self.snackbar.open = True
                     self.snackbar.update()
@@ -175,6 +210,8 @@ class Login(ft.Container):
                 else:
                     # Create new session
                     self.page.session.set("session", user)
+                    logger.info("Sesión iniciada con éxito.")
+                    logger.debug(f" >>> Usuario: '{mask_email(user.email)}' BIENVENIDO.")
 
                     # Report page loading
                     self.page.overlay.append(
