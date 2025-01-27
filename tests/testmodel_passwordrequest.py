e6a8d64de502a160e231d3acd287ea3f5c69f87e
Joan <j.pastor1591@gmail.com>
2025-01-27 17:32:10 +0100

    Refactoring name of files

--- tests/testmodel_passwordrequest.py
+++ tests/testmodel_passwordrequest.py
@@ -35,9 +35,11 @@ class TestPasswordRequest(unittest.TestCase):
 
     def test_expired_request(self) -> None:
         logger.info(">>> Starting PASSWORD REQUEST expired request test...")
-        self.assertGreater(self.request_01.created, self.expired_date, f"Should be '{self.expired_date.strftime(
-            "%Y-%m-%dT%H:%M:%S"
-        )}'...")
-        self.assertLess(self.request_02.created, self.future_date, f"Should be '{self.future_date.strftime(
-            "%Y-%m-%dT%H:%M:%S"
-        )}'...")
+        self.assertGreater(
+            self.request_01.created,
+            self.expired_date,
+            f"Should be '{self.expired_date.strftime('%Y-%m-%dT%H:%M:%S')}'...")
+        self.assertLess(
+            self.request_02.created,
+            self.future_date,
+            f"Should be '{self.future_date.strftime('%Y-%m-%dT%H:%M:%S')}'...")
