af80c10e925ecc6e332ddeeaaf8b1c2f3a8a002b
Joan <j.pastor1591@gmail.com>
2025-01-30 20:20:37 +0100

    Added 'value' to builder

--- interface/controls/my_switch.py
+++ interface/controls/my_switch.py
@@ -1,13 +1,16 @@
 import flet as ft
+from typing import Callable
 
 from shared.utils.colors import *
 
 
 class CustomSwitch(ft.Container):
-    def __init__(self, title: str, width: int) -> None:
+    def __init__(self, title: str, width: int | None = None, expand: bool = False,
+                 on_change: Callable[[ft.ControlEvent], None] | None = None, value: bool = False) -> None:
         super().__init__()
 
         self.width = width
+        self.expand = expand
 
         self.content = ft.Row(
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
@@ -18,7 +21,9 @@ class CustomSwitch(ft.Container):
                     size=16
                 ),
                 ft.Switch(
+                    value=value,
                     width=36,
+                    on_change=on_change,
                     active_track_color=neutralSuccessMedium,
                     track_color={
                         ft.ControlState.PRESSED: ft.Colors.PINK
