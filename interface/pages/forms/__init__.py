5bd82fd579e308ef48f568c6815d91bcd5e89a6b
Joan <j.pastor1591@gmail.com>
2025-01-30 12:42:24 +0100

    Added styles to Init file

--- interface/pages/forms/__init__.py
+++ interface/pages/forms/__init__.py
@@ -1,3 +1,4 @@
-from .delete_form import DeleteForm
+from .delete_form import DeleteFormStyle, DeleteForm
+from .generate_form import GenerateFormStyle, GenerateForm
 
-__all__ = ["DeleteForm"]
+__all__ = ["DeleteFormStyle", "DeleteForm", "GenerateFormStyle", "GenerateForm"]
