f153bd85e7dee13d0c3a57c6169c606914355138
Joan <j.pastor1591@gmail.com>
2025-02-06 20:14:09 +0100

    Added AddForm to init file

--- interface/pages/forms/__init__.py
+++ interface/pages/forms/__init__.py
@@ -1,4 +1,5 @@
 from .delete_form import DeleteFormStyle, DeleteForm
 from .generate_form import GenerateFormStyle, GenerateForm
+from .add_form import AddFormStyle, AddForm
 
-__all__ = ["DeleteFormStyle", "DeleteForm", "GenerateFormStyle", "GenerateForm"]
+__all__ = ["DeleteFormStyle", "DeleteForm", "GenerateFormStyle", "GenerateForm", "AddFormStyle", "AddForm"]
