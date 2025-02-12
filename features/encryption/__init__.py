081658055ecf4f483a803ebfd0e7577a49052f79
Joan <j.pastor1591@gmail.com>
2025-02-12 09:47:35 +0100

    Key file creation implemented

---
+++ features/encryption/__init__.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import os
+from cryptography.fernet import Fernet
+
+from shared.logger_setup import main_logger as logger
+
+
+def create_key():
+    new_key = Fernet.generate_key()
+    try:
+        with open(f"{key_path}/key.key", "xb") as key_file:
+            key_file.write(new_key)
+
+    except PermissionError as permission:
+        logger.error(f"{type(permission).__name__} ::: No se disponen de permisos para la creación del archivo 'key'.")
+
+    except FileExistsError as already_exists:
+        logger.error(f"{type(already_exists).__name__} ::: El archivo 'key' ya existe.")
+
+    except Exception as unknown:
+        logger.error(f"{type(unknown).__name__} ::: Un error inesperado a ocurrido al tratar de crear el archivo "
+                     f"'key'.")
+
+
+# Create key path
+key_path = Path(__file__).parent
+
+
+# Check if key already exists:
+if not "key.key" in os.listdir(key_path):
+    create_key()
