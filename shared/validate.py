ebfd33318540752bd2d8ba5569c097cbcd667e1d
Joan <j.pastor1591@gmail.com>
2025-02-18 08:52:31 +0100

    Added class representation

--- shared/validate.py
+++ shared/validate.py
@@ -86,7 +86,7 @@ class Validate:
     @staticmethod
     def is_valid_date(new_date: str) -> bool:
         """
-        In this case, date must have the next format: 'dd-mm-yyyy'.
+        Date must have the next format: 'dd-mm-yyyy'.
         :param new_date: date user input
         :return: boolean
         """
@@ -94,3 +94,6 @@ class Validate:
         sequence = (r"^(((0[1-9]|[12][0-9]|3[01])-(0[13578]|1[02])|(0[1-9]|[12][0-9]|30)-(0[469]|11)|(0[1-9]|1"
                     r"\d|2[0-8])-02)-\d{4}|29-02-(\d{2}(0[48]|[2468][048]|[13579][26])|([02468][048]|[1359][26])00))$")
         return True if re.match(sequence, new_date) else False
+
+    def __str__(self) -> str:
+        return f"<class Validate({dir(self)})>"
