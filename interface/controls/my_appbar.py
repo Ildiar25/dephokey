a2257cd52faf73f70917dc4c608aca980295d645
Joan <j.pastor1591@gmail.com>
2025-02-18 12:16:22 +0100

    Added settings forms

--- interface/controls/my_appbar.py
+++ interface/controls/my_appbar.py
@@ -4,6 +4,7 @@ import flet as ft
 import time
 
 from interface.controls.custom_searchbar import CustomSearchBar
+from interface.controls import CustomElevatedButton, CustomTextField
 from interface.pages.body_content import BodyContent
 from interface.pages import LoadPage  # ---> ImportError (Circular import)
 
@@ -11,17 +12,36 @@ from shared.utils.colors import *
 
 
 class CustomAppbar(ft.AppBar):
-    def __init__(self, content: BodyContent,
+    def __init__(self,
+                 page: ft.Page,
+                 content: BodyContent,
                  search_bar: bool = False,
                  find_function: Callable[[ft.ControlEvent], None] | None = None) -> None:
         super().__init__()
 
         # General settings
+        self.page = page
         self.visible = False
         self.search_bar = search_bar
         self.toolbar_height = 79
         self.settings_content = content
         self.look_for_elements = find_function
+
+        # Settings attributes
+        self.fullname = CustomTextField(
+            value=self.page.session.get("session").fullname,
+            expand=True,
+        )
+        self.email = CustomTextField(
+            value=self.page.session.get("session").email,
+            expand=True
+        )
+        self.password = CustomTextField(
+            value="oooooooooooo",
+            expand=True,
+            password=True,
+            read_only=True,
+        )
 
         # Design settings
         self.bgcolor = bgAppbarColor
@@ -67,7 +87,139 @@ class CustomAppbar(ft.AppBar):
     def settings(self, _: ft.ControlEvent) -> None:
         self.settings_content.controls[0].controls[0].value = "Configuración"
         self.settings_content.controls[0].controls[1].controls = []
-        self.settings_content.controls[1].controls = []
+        self.settings_content.controls[1].controls = [
+
+            ft.Row(
+                spacing=32,
+                controls=[
+                    ft.Container(
+                        height=334,
+                        expand=True,
+                        bgcolor=bgGeneralFormColor,
+                        border_radius=4,
+                        padding=24,
+                        shadow=ft.BoxShadow(blur_radius=0.9, offset=(0.0, 0.5),
+                                    color=ft.Colors.with_opacity(opacity=0.3, color=neutral80)),
+                        content=ft.Column(
+                            spacing=24,
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                                ft.Row(
+                                    # height=29,
+                                    controls=[
+                                        ft.Text(
+                                            "Información general",
+                                            font_family="AlberSansR",
+                                            size=24,
+                                            color=accentTextColor
+                                        ),
+                                    ]
+                                ),
+                                ft.Row(
+                                    # height=140,
+                                    controls=[
+                                        ft.Column(
+                                            expand=True,
+                                            spacing=8,
+                                            controls=[
+                                                ft.Text(
+                                                    "Nombre completo:",
+                                                    font_family="AlbertSansL",
+                                                    size=16,
+                                                    color=primaryTextColor
+                                                ),
+                                                ft.Row(controls=[self.fullname]),
+                                                ft.Text(
+                                                    "Correo electrónico:",
+                                                    font_family="AlbertSansL",
+                                                    size=16,
+                                                    color=primaryTextColor
+                                                ),
+                                                ft.Row(controls=[self.email])
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                ft.Row(
+                                    alignment=ft.MainAxisAlignment.END,
+                                    controls=[
+                                        CustomElevatedButton(
+                                            name="Guardar",
+                                            width=85,
+                                            bg_color=bgEButtonColor,
+                                            foreground_color=tertiaryTextColor,
+                                            border_size=-1
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    ),
+                    ft.Container(
+                        height=334,
+                        expand=True,
+                        bgcolor=bgGeneralFormColor,
+                        border_radius=4,
+                        padding=24,
+                        shadow=ft.BoxShadow(blur_radius=0.9, offset=(0.0, 0.5),
+                                            color=ft.Colors.with_opacity(opacity=0.3, color=neutral80)),
+                        content=ft.Column(
+                            spacing=24,
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                                ft.Row(
+                                    # height=29,
+                                    controls=[
+                                        ft.Text(
+                                            "Seguridad",
+                                            font_family="AlberSansR",
+                                            size=24,
+                                            color=accentTextColor
+                                        ),
+                                    ]
+                                ),
+                                ft.Row(
+                                    # height=140,
+                                    controls=[
+                                        ft.Column(
+                                            expand=True,
+                                            spacing=8,
+                                            controls=[
+                                                ft.Text(
+                                                    "Contraseña:",
+                                                    font_family="AlbertSansL",
+                                                    size=16,
+                                                    color=primaryTextColor
+                                                ),
+                                                ft.Row(controls=[self.password]),
+                                                ft.Text(
+                                                    "",
+                                                    font_family="AlbertSansL",
+                                                    size=16,
+                                                    color=primaryTextColor
+                                                ),
+                                                ft.Row(controls=[CustomTextField(disabled=True, border_width=0)])
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                ft.Row(
+                                    alignment=ft.MainAxisAlignment.END,
+                                    controls=[
+                                        CustomElevatedButton(
+                                            name="Cambiar contraseña",
+                                            foreground_color=secondaryTextColor,
+                                            # on_click=lambda _: self.page.close(self),
+                                            border_size=-1
+                                        )
+                                    ]
+                                )
+                            ]
+                        )
+                    )
+                ]
+            )
+        ]
         self.settings_content.update()
 
     def logout(self, _: ft.ControlEvent) -> None:
