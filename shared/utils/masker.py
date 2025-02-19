d1ba1630c04e4f337d8026f7339233f06225a7dd
Joan <j.pastor1591@gmail.com>
2025-02-19 16:29:01 +0100

    Change * by •

--- shared/utils/masker.py
+++ shared/utils/masker.py
@@ -2,7 +2,7 @@
 
 def mask_email(email: str) -> str:
     local, domain = email.split("@")
-    masked = "*" * len(local)
+    masked = "•" * len(local)
     return masked + "@" + domain
 
 
@@ -11,25 +11,25 @@ def mask_username(username: str) -> str:
         return mask_email(username)
 
     else:
-        masked = "*" * len(username[3:])
+        masked = "•" * len(username[3:])
         return username[:3] + masked
 
 
 def mask_password(passsword: str) -> str:
-    return "*" * len(passsword)
+    return "•" * len(passsword)
 
 
 def mask_number(number: str) -> str:
-    masked = "*" * len(number[:-4])
+    masked = "•" * len(number[:-4])
     return masked + number[-4:]
 
 
 def mask_phone(phone: str) -> str:
-    masked = "*" * len(phone[:-3])
+    masked = "•" * len(phone[:-3])
     return masked + phone[-3:]
 
 
 def mask_text(text: str) -> str:
     if len(text) >= 20:
-        return "*" * 17 + "..."
-    return "*" * len(text)
+        return "•" * 17 + "..."
+    return "•" * len(text)
