7140d2c7cdc5b62499236cb47850e43f0df08cc0
Joan <j.pastor1591@gmail.com>
2025-02-19 16:29:58 +0100

    Starting to prepare elements

--- interface/pages/reset_password.py
+++ interface/pages/reset_password.py
@@ -1,5 +1,6 @@
 import flet as ft
 
+from interface.controls import CustomTextField, CustomElevatedButton
 from shared.utils.colors import *
 
 
@@ -11,11 +12,23 @@ class ResetPasswordPage(ft.Container):
         self.page = page
 
         # ResetPassword attributes
+        self.main_field = CustomTextField(
+            label="Correo Electrónico",
+            on_change=None
+        )
+        self.submit_email = CustomElevatedButton(
+            name="¡Vamos!",
+            width=85,
+            bg_color=bgEButtonColor,
+            foreground_color=tertiaryTextColor,
+            on_click=None,
+            border_size=-1
+        )
 
         # Page design
         self.expand = True
         self.bgcolor = bgGeneralFormColor
-        self.padding = ft.padding.only(32, 56, 32)
+        self.padding = ft.padding.only(32, 56, 32, 56)
 
         # Body content
         self.content = ft.Column(
@@ -29,6 +42,12 @@ class ResetPasswordPage(ft.Container):
                             highlight_color=neutral20,
                             hover_color=neutral10
                         )
+                    ]
+                ),
+                ft.Row(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    controls=[
+                        self.main_field, self.submit_email
                     ]
                 )
             ]
