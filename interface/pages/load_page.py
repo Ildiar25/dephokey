150b51b689970ab3cd0f95685f62658a7794a8a6
Joan <j.pastor1591@gmail.com>
2025-02-19 18:28:30 +0100

    Updated image background name

--- interface/pages/load_page.py
+++ interface/pages/load_page.py
@@ -12,7 +12,7 @@ class LoadPage(ft.Container):
         self.alignment = ft.alignment.top_center
         self.expand = True
         self.bgcolor = ft.Colors.with_opacity(0.3, neutral00)
-        self.image = ft.DecorationImage(src="interface/assets/blurry-bgimage.png", fit=ft.ImageFit.COVER)
+        self.image = ft.DecorationImage(src="interface/assets/bgimage-load-page.png", fit=ft.ImageFit.COVER)
 
         # Content
         self.content = ft.ProgressBar(color=secondaryCorporateColor)
