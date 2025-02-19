c29d020e32cb8ef9d5d2e2b5afa271f00b99b634
Joan <j.pastor1591@gmail.com>
2025-02-19 20:43:53 +0100

    Prepare main functions

--- interface/pages/forms/add_form.py
+++ interface/pages/forms/add_form.py
@@ -7,7 +7,6 @@ from data.db_orm import session
 
 from features.models.user import User
 
-from interface.controls.date_picker import DatePicker
 from interface.controls import CustomElevatedButton, CustomTextField
 
 from shared.utils.colors import *
@@ -29,7 +28,7 @@ class AddForm(ft.AlertDialog):
         self.user: User = self.page.session.get("session")
         self.submit_button = CustomElevatedButton(
             name="Aceptar", bg_color=bgEButtonColor, foreground_color=tertiaryTextColor,
-            border_size=-1, width=84, disabled=False
+            border_size=-1, width=84, disabled=True
         )
         self.cancel_button = CustomElevatedButton(
             name="Cancelar", foreground_color=secondaryTextColor,
@@ -41,13 +40,20 @@ class AddForm(ft.AlertDialog):
 
             case AddFormStyle.SITE:
                 # Site attributes
-                self.site_name = CustomTextField(label="Nombre del sitio (opcional)", width=250)
-                self.site_address = CustomTextField(label="Dirección web", prefix_text="http://", width=250)
-                self.site_username = CustomTextField(label="Usuario", width=250)
-                self.site_password = CustomTextField(label="Contraseña", password=True, can_reveal_password=True,
-                                                     width=250)
+                self.site_name = CustomTextField(
+                    label="Nombre del sitio (opcional)", width=250, on_change=self.toggle_submit_button_state
+                )
+                self.site_address = CustomTextField(
+                    label="Dirección web", prefix_text="http://", width=250, on_change=self.toggle_submit_button_state
+                )
+                self.site_username = CustomTextField(
+                    label="Usuario", width=250, on_change=self.toggle_submit_button_state
+                )
+                self.site_password = CustomTextField(
+                    label="Contraseña", password=True, can_reveal_password=True, width=250,
+                    on_change=self.toggle_submit_button_state)
 
-                self.submit_button.on_click = lambda _:print(self.site_address.value)
+                self.submit_button.on_click = self.add_site
 
                 # Site content
                 self.bodycontent = ft.Container(
@@ -83,11 +89,22 @@ class AddForm(ft.AlertDialog):
 
             case AddFormStyle.CREDITCARD:
                 # CreditCard attributes
-                self.creditcard_cardholder = CustomTextField(label="Titular", width=250)
-                self.creditcard_alias = CustomTextField(label="Alias (opcional)", width=250)
-                self.creditcard_number = CustomTextField(label="Número", width=320)
-                self.creditcard_cvc = CustomTextField(label="CVC", width=64)
-                self.creditcard_expires_date = CustomTextField(label="Caducidad", width=115)
+                self.creditcard_cardholder = CustomTextField(
+                    label="Titular", width=250, on_change=self.toggle_submit_button_state
+                )
+                self.creditcard_alias = CustomTextField(
+                    label="Alias (opcional)", width=250, on_change=self.toggle_submit_button_state
+                )
+                self.creditcard_number = CustomTextField(
+                    label="Número", width=320, on_change=self.toggle_submit_button_state
+                )
+                self.creditcard_cvc = CustomTextField(
+                    label="CVC", width=64, on_change=self.toggle_submit_button_state
+                )
+                self.creditcard_expires_date = CustomTextField(
+                    label="Caducidad", width=115, on_change=self.toggle_submit_button_state
+                )
+                self.submit_button.on_click = self.add_creditcard
 
 
                 # CreditCard content
@@ -124,9 +141,14 @@ class AddForm(ft.AlertDialog):
 
             case AddFormStyle.NOTE:
                 # Note attributes
-                self.note_title = CustomTextField(label="Título (opcional)", width=520)
-                self.note_content = CustomTextField(label="Contenido", max_lines=2, multiline=True, width=520,
-                                                    min_lines=2)
+                self.note_title = CustomTextField(
+                    label="Título (opcional)", width=520, on_change=self.toggle_submit_button_state
+                )
+                self.note_content = CustomTextField(
+                    label="Contenido", max_lines=2, multiline=True, width=520, min_lines=2,
+                    on_change=self.toggle_submit_button_state
+                )
+                self.submit_button.on_click = self.add_note
 
                 # Note content
                 self.bodycontent = ft.Container(
@@ -194,3 +216,39 @@ class AddForm(ft.AlertDialog):
         # Form design
         self.shape = ft.RoundedRectangleBorder(4)
         self.bgcolor = bgGeneralFormColor
+
+    def toggle_submit_button_state(self, cursor: ft.ControlEvent) -> None:
+        # Site status
+        if self.addform_style == AddFormStyle.SITE:
+            if cursor and all([self.site_address.value, self.site_username.value, self.site_password.value]):
+                self.submit_button.disabled = False
+            else:
+                self.submit_button.disabled = True
+
+        # CreditCard status
+        if self.addform_style == AddFormStyle.CREDITCARD:
+            if cursor and all([self.creditcard_cardholder.value, self.creditcard_number.value,
+                               self.creditcard_cvc.value, self.creditcard_expires_date.value]):
+                self.submit_button.disabled = False
+            else:
+                self.submit_button.disabled = True
+
+        # Note status
+        if self.addform_style == AddFormStyle.NOTE:
+            if cursor and self.note_content.value:
+                self.submit_button.disabled = False
+            else:
+                self.submit_button.disabled = True
+        self.submit_button.update()
+
+    def add_site(self, _: ft.ControlEvent) -> None:
+        print("Site added")
+        raise NotImplementedError("Implementar 'añadir_sitio_web' a la base de datos.")
+
+    def add_creditcard(self, _: ft.ControlEvent) -> None:
+        print("Creditcard added")
+        raise NotImplementedError("Implementar 'añadir_tarjeta_de_crédito' a la base de datos.")
+
+    def add_note(self, _: ft.ControlEvent) -> None:
+        print("Note added")
+        raise NotImplementedError("Implementar 'añadir_nota_segura' a la base de datos.")
