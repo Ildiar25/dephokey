712300fb6fdc7e8f18a973113248a1f6b224fa54
Joan <j.pastor1591@gmail.com>
2025-02-20 20:39:45 +0100

    Improve FORM imports

--- interface/pages/forms/__init__.py
+++ interface/pages/forms/__init__.py
@@ -1,5 +1,7 @@
 from .delete_form import DeleteFormStyle, DeleteForm
 from .generate_form import GenerateFormStyle, GenerateForm
 from .add_form import AddFormStyle, AddForm
+from .change_password_form import ChangePasswordForm
 
-__all__ = ["DeleteFormStyle", "DeleteForm", "GenerateFormStyle", "GenerateForm", "AddFormStyle", "AddForm"]
+__all__ = ["DeleteFormStyle", "DeleteForm", "GenerateFormStyle", "GenerateForm", "AddFormStyle", "AddForm",
+           "ChangePasswordForm"]
