9d923e56a8fa6db11c9163405e15a97753c175ba
Joan <j.pastor1591@gmail.com>
2025-02-20 20:39:57 +0100

    Improve PAGES imports

--- interface/pages/__init__.py
+++ interface/pages/__init__.py
@@ -1,9 +1,8 @@
-from .body_content import BodyContent
-from .load_page import LoadPage
 from .login import Login
 from .signup import Signup
-from .home import Home
-from .admin import Admin
 from .reset_password import ResetPasswordPage
+from .dashboard import Dashboard
 
-__all__ = ["Admin", "Login", "Signup", "Home", "LoadPage", "BodyContent", "ResetPasswordPage"]
+__all__ = [
+    "Login", "Signup", "ResetPasswordPage", "Dashboard"
+]
