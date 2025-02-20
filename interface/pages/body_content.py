144d90b6c4d896aa0d917cc5003b19338aed2a38
Joan <j.pastor1591@gmail.com>
2025-02-20 19:38:16 +0100

    Refactored 'body_content': Now it has control about showed elements

--- interface/pages/body_content.py
+++ interface/pages/body_content.py
@@ -1,40 +1,84 @@
 import flet as ft
-from typing import List
+from typing import List, Union
+from enum import Enum
 
-
+from interface.controls.snackbar import Snackbar
+from interface.pages.content_settings import SettingsPage
+from interface.pages.widgets import SiteWidget, NoteWidget
 from shared.utils.colors import *
 
 
+class ContentStyle(Enum):
+    HOME = "home"
+    SITES = "sites"
+    CREDITCARDS = "creditcards"
+    NOTES = "notes"
+    ABOUT = "about"
+    SETTINGS = "settings"
+    EMPTY = "empty"
+
+
 class BodyContent(ft.Column):
-    def __init__(self, title: str = "'no_title'", buttons: List[ft.Control] | None = None,
-                 widgets: List[ft.Control] | None = None, visible: bool = True) -> None:
+    def __init__(self, page: ft.Page, snackbar: Snackbar, style: ContentStyle = ContentStyle.EMPTY,
+                 title: str = "", buttons: Union[List[ft.Control], None] = None) -> None:
         super().__init__()
 
-        # Main elements
-        self.title = ft.Text(
-            title,
-            font_family="AlbertSansB", color=primaryTextColor, size=24
+        # General attributes
+        self.page = page
+        self.snackbar = snackbar
+        self.style = style
+
+        # BodyContent attributes
+        self.user = self.page.session.get("session")
+        self.header = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            controls=[
+                ft.Text(value=title, font_family="AlbertSansB", color=primaryTextColor, size=24),
+                ft.Row(spacing=16, controls=buttons)
+            ]
         )
-        self.buttons = ft.Row(
-            controls=buttons
-        )
+        self.body = ft.Row(expand=True, wrap=True, spacing=16)
+        self.style = None
 
         # Body design
         self.spacing = 32
-        self.visible = visible
         self.scroll = ft.ScrollMode.AUTO
         self.expand = True
 
         # Body content
         self.controls = [
-            ft.Row(
-                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                controls=[self.title, self.buttons]
-            ),
-            ft.Row(
-                expand=True,
-                wrap=True,
-                spacing=16,
-                controls=widgets
-            )
+            self.header,
+            self.body
         ]
+
+        self.update_appearance()
+
+    def change_content(self, title: str, style: ContentStyle, buttons: Union[List[ft.Control], None] = None) -> None:
+        self.style = style
+        self.header.controls[0].value = title
+        self.header.controls[1].controls = buttons
+        self.update_appearance()
+
+    def update_appearance(self) -> None:
+        match self.style:
+            case ContentStyle.HOME:
+                self.body.controls = []
+
+            case ContentStyle.SITES:
+                self.body.controls = [SiteWidget(site, self.page) for site in self.user.sites]
+
+            case ContentStyle.CREDITCARDS:
+                self.body.controls = []
+
+            case ContentStyle.NOTES:
+                self.body.controls = [NoteWidget(note, self.page) for note in self.user.notes]
+
+            case ContentStyle.ABOUT:
+                self.body.controls = []
+
+            case ContentStyle.SETTINGS:
+                self.body.controls = [SettingsPage(self.page, self.snackbar)]
+
+            case ContentStyle.EMPTY:
+                self.header.controls[1].controls.clear()
+                self.body.controls.clear()
