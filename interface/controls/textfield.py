0d2e6458f75db4b3788d4e34b98645609f638ba2
Joan <j.pastor1591@gmail.com>
2025-02-21 09:04:02 +0100

    Added 'show_error' & 'reset_error' methods: Now, textfield can show a little error message and delete it.

--- interface/controls/textfield.py
+++ interface/controls/textfield.py
@@ -5,17 +5,26 @@ from shared.utils.colors import *
 
 
 class CustomTextField(ft.TextField):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, error: str | None = None, **kwargs) -> None:
         super().__init__(**kwargs)
+
+        # Specific settings
+        self.error_text = error
 
         # Text design settings
         self.cursor_color = cursorTextfieldColor
         self.selection_color = selectCursorTextfieldColor
-        self.label_style = ft.TextStyle(
-            color=labelTextfieldColor
-        )
+        self.label_style = ft.TextStyle(color=labelTextfieldColor)
 
         # Field design settings
         self.border_radius = 4
         self.border_color = staticBorderTextfieldColor
         self.focused_border_color = selectedBorderTextfieldColor
+
+    def reset_error(self) -> None:
+        self.error_text = None
+        self.update()
+
+    def show_error(self, msg: str) -> None:
+        self.error_text = msg
+        self.update()
