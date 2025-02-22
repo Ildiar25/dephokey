b84a095ef2a999710250b950ca28491e223bda7d
Joan <j.pastor1591@gmail.com>
2025-02-22 10:22:07 +0100

    Refactored 'SiteWidget' with 'IconLink' & 'TextLink' classes. Added 'update_appearance' attribute

--- interface/pages/widgets/site_widget.py
+++ interface/pages/widgets/site_widget.py
@@ -1,4 +1,5 @@
 import flet as ft
+from typing import Callable
 import time
 
 from data.db_orm import session
@@ -6,6 +7,7 @@ from data.db_orm import session
 from features.models import Site
 from features.encryption.core import decrypt_data
 
+from interface.controls import TextLink, IconLink, IconLinkStyle
 from interface.pages.forms import DeleteFormStyle, DeleteForm
 
 from shared.utils.masker import mask_password
@@ -13,12 +15,13 @@ from shared.utils.colors import *
 
 
 class SiteWidget(ft.Card):
-    def __init__(self, site: Site, page: ft.Page) -> None:
+    def __init__(self, site: Site, page: ft.Page, update_appearance: Callable[[], None]) -> None:
         super().__init__()
 
         # General attributes
         self.site = site
         self.page = page
+        self.update_appearance = update_appearance
 
         # Widget settings
         self.width = 365
@@ -26,36 +29,11 @@ class SiteWidget(ft.Card):
         self.elevation = 2
 
         # SiteWidget elements
-        self.title = ft.Text(
-            self.site.name if self.site.name else "Sin título",
-            font_family="AlbertSansB",
-            size=18,
-            color=titleSiteWidgetColor
-        )
-        self.site_link = ft.Container(
-            on_hover=self.focus_link,
-            on_click=lambda _: self.page.launch_url(self.site.address),
-            content=ft.Text(
-                self.site.address,
-                color=textSiteWidgetColor
-            )
-        )
-        self.site_username = ft.Text(
-            self.site.username,
-            color=textSiteWidgetColor
-        )
-        self.site_password = ft.Text(
-            mask_password(decrypt_data(self.site.encrypted_password))
-        )
-        self.copy_button = ft.Container(
-            visible=False,
-            on_click=lambda _:self.page.set_clipboard(self.site_password.value),
-            padding=ft.padding.only(3),
-            content=ft.Icon(
-                ft.Icons.COPY_ROUNDED,
-                size=18
-            )
-        )
+        self.site_title = ft.Text(self.site.name if self.site.name else "Sin título", font_family="AlbertSansB",
+                                  size=18, color=titleSiteWidgetColor)
+        self.site_link = TextLink(text=self.site.address, function=lambda _: self.page.launch_url(self.site.address))
+        self.site_username = ft.Text(self.site.username, color=textSiteWidgetColor)
+        self.site_password = ft.Text(mask_password(decrypt_data(self.site.encrypted_password)))
 
         # Widget design
         self.color = bgSiteWidgetColor
@@ -73,26 +51,14 @@ class SiteWidget(ft.Card):
                     ft.Row(
                         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                         controls=[
-                            self.title,
+                            self.site_title,
                             ft.Container(
                                 content=ft.Row(
                                     spacing=8,
                                     controls=[
-                                        ft.Container(
-                                            on_hover=self.toggle_icon_color,
-                                            content=ft.Icon(
-                                            ft.Icons.EDIT_OUTLINED,
-                                            color=iconAccentSiteWidgetColor
-                                            )
-                                        ),
-                                        ft.Container(
-                                            on_hover=self.toggle_icon_color,
-                                            on_click=self.open_delete_form,
-                                            content=ft.Icon(
-                                                ft.Icons.DELETE_OUTLINE_ROUNDED,
-                                                color=iconAccentSiteWidgetColor
-                                            )
-                                        )
+                                        IconLink(ft.Icons.EDIT_OUTLINED, IconLinkStyle.LIGHT),
+                                        IconLink(ft.Icons.DELETE_OUTLINED, IconLinkStyle.LIGHT,
+                                                 function=self.open_delete_form)
                                     ]
                                 )
                             )
@@ -105,7 +71,7 @@ class SiteWidget(ft.Card):
                         controls=[
                             ft.Icon(
                                 ft.Icons.LINK_ROUNDED,
-                                color=iconSiteWidgetColor
+                                color=primaryIconColor
                             ),
                             self.site_link
                         ]
@@ -115,7 +81,7 @@ class SiteWidget(ft.Card):
                         controls=[
                             ft.Icon(
                                 ft.Icons.ACCOUNT_CIRCLE_ROUNDED,
-                                color=iconSiteWidgetColor
+                                color=primaryIconColor
                             ),
                             self.site_username
                         ]
@@ -124,51 +90,28 @@ class SiteWidget(ft.Card):
                         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
                         controls=[
                             ft.Container(
+                                on_hover=self.show_password,
                                 content=ft.Row(
                                     spacing=8,
                                     controls=[
                                         ft.Icon(
                                             ft.Icons.PASSWORD_ROUNDED,
-                                            color=iconSiteWidgetColor
+                                            color=primaryIconColor
                                         ),
-                                        self.site_password,
-                                        self.copy_button
+                                        self.site_password
                                     ]
                                 )
                             ),
-                            ft.Container(
-                                on_hover=self.focus_link,
-                                on_click=self.show_password,
-                                tooltip="Muestra la contraseña durante 3 segundos",
-                                content=ft.Text(
-                                    "ver contraseña",
-                                    color=accentTextColor
-                                )
-                            )
+                            IconLink(ft.Icons.COPY_ROUNDED, style=IconLinkStyle.LIGHT, function=self.copy_text,
+                                     tooltip="copiar contraseña")
                         ]
                     )
                 ]
             )
         )
 
-    @staticmethod
-    def focus_link(cursor: ft.ControlEvent) -> None:
-        if cursor and cursor.control.content.color == accentTextColor:
-            cursor.control.content.color = textSiteWidgetColor
-        else:
-            cursor.control.content.color = accentTextColor
-        cursor.control.update()
-
-    @staticmethod
-    def toggle_icon_color(cursor: ft.ControlEvent) -> None:
-        if cursor and cursor.control.content.color == iconAccentSiteWidgetColor:
-            cursor.control.content.color = iconSiteWidgetColor
-        else:
-            cursor.control.content.color = iconAccentSiteWidgetColor
-        cursor.control.update()
-
     def copy_text(self, cursor: ft.ControlEvent) -> None:
-        self.page.set_clipboard(self.site_password.value)
+        self.page.set_clipboard(decrypt_data(self.site.encrypted_password))
         cursor.control.badge = ft.Badge(
             text="Copiado!",
             bgcolor=ft.Colors.with_opacity(opacity=0.5, color=neutral80),
@@ -180,18 +123,10 @@ class SiteWidget(ft.Card):
         cursor.control.update()
 
     def show_password(self, cursor: ft.ControlEvent) -> None:
-        if cursor:
+        if cursor and self.site_password.value == mask_password(decrypt_data(self.site.encrypted_password)):
             self.site_password.value = decrypt_data(self.site.encrypted_password)
-            self.site_password.update()
-            self.copy_button.visible = True
-            # self.copy_button.disabled = False
-            self.copy_button.update()
-            time.sleep(3)
+        else:
             self.site_password.value = mask_password(decrypt_data(self.site.encrypted_password))
-            self.copy_button.visible = False
-            # self.copy_button.disabled = True
-            self.copy_button.update()
-
         self.site_password.update()
 
     def open_delete_form(self, _: ft.ControlEvent) -> None:
@@ -201,3 +136,4 @@ class SiteWidget(ft.Card):
         # New query
         session.query(Site).filter(Site.id == self.site.id).delete()
         session.commit()
+        self.update_appearance()
