2e32626c0bfa8868501bed6ccb0de76e40304cb7
Joan <j.pastor1591@gmail.com>
2025-02-25 11:54:52 +0100

    Created new 'SitesContent' class: It has its own item management and updates it when its necessary

---
+++ interface/pages/page_content/sites_content.py
@@ -0,0 +1,39 @@
+import flet as ft
+from typing import Callable, List
+
+from data.db_orm import session
+
+from features.models.user import User
+from features.models import Site
+
+from interface.controls import Snackbar
+from interface.pages.widgets import SiteWidget
+
+
+class SitesPage(ft.Column):
+    def __init__(self, page: ft.Page, snackbar: Snackbar, update_changes: Callable[[], None]) -> None:
+        super().__init__()
+
+        # General attributes
+        self.page = page
+        self.snackbar = snackbar
+        self.update_changes = update_changes
+
+        # SitesPage attributes
+        self.user: User = self.page.session.get("session")
+        self.sites = []
+
+        # Body content
+        self.sites_row = ft.Row(wrap=True, spacing=16)
+        self.controls = [self.sites_row]
+
+        self.update_content()
+
+    def update_content(self) -> None:
+        self.sites = session.query(Site).filter_by(user_id=self.user.id).all()
+        self.__populate_row(self.sites)
+
+    def __populate_row(self, sites: List[Site]) -> None:
+        self.sites_row.controls.clear()
+        for site in sites:
+            self.sites_row.controls.append(SiteWidget(site, self.page, self.update_changes))
