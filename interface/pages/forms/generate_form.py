dff6e08f3b1d51592708612f1a31afa996e167f8
Joan <j.pastor1591@gmail.com>
2025-02-26 10:13:16 +0100

    Refactored 'GenerateForm': Added new 'FormStyles' & inheritance from 'BaseForm'

--- interface/pages/forms/generate_form.py
+++ interface/pages/forms/generate_form.py
@@ -1,245 +1,168 @@
-import flet as ft
-from enum import Enum
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 from faker import Faker
+import flet as ft
 import random
-import time
 
-from interface.controls import CustomElevatedButton, CustomSwitch, CustomTextField
+from .base_form import BaseForm, FormStyle
+from interface.controls import CustomElevatedButton, ButtonStyle, CustomSwitch, CustomTextField, IconLink, IconLinkStyle
 
+from shared.logger_setup import main_logger as logger
 from shared.utils.colors import *
 
 
-class GenerateFormStyle(Enum):
-    PASSWORD = "password"
-    NUMBER = "number"
-
-
-class GenerateForm(ft.AlertDialog):
-    def __init__(self, page: ft.Page, title: str, generate_style: GenerateFormStyle) -> None:
+class GenerateForm(BaseForm):
+    def __init__(self, title: str, page: ft.Page, style: FormStyle) -> None:
         super().__init__()
 
         # General attributes
         self.page = page
-        self.generate_style = generate_style
-        self.submit_button = CustomElevatedButton(
-            name="Generar", bg_color=bgEButtonColor, foreground_color=tertiaryTextColor,
-            border_size=-1, expand=True, disabled=False
-        )
-        self.result = CustomTextField(expand=True, read_only=True)
-        self.copybutton = ft.IconButton(
-            ft.Icons.COPY_ROUNDED,
-            icon_color=primaryCorporateColor,
-            on_click=self.copy_text,
-            highlight_color=selectedIconGeneralFormColor,
-            hover_color=hoverIconGeneralFormColor,
-            tooltip="Copiar al portapapeles"
-        )
+        self.style = style
 
-        # Content according to style
-        match generate_style:
-            case GenerateFormStyle.PASSWORD:
-
-                # GeneratePassword attributes
-                self.switches = [
-                    CustomSwitch(title="Minúsculas", width=240, on_change=self.toggle_generate_button_state,
-                                 value=True),
-                    CustomSwitch(title="Mayúsculas", width=240, on_change=self.toggle_generate_button_state),
-                    CustomSwitch(title="Números", width=240, on_change=self.toggle_generate_button_state),
-                    CustomSwitch(title="Caracteres Especiales", width=240, on_change=self.toggle_generate_button_state)
-                ]
-                self.max_input = CustomTextField(width=76, max_length=3, on_change=self.change_number_input)
-                self.submit_button.on_click = self.generate_password
-                self.bodycontent = ft.Container(
-                    width=524,
-                    height=300,
-                    content=ft.Column(
-                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                        spacing=22,
-                        controls=[
-                            ft.Row(
-                                wrap=True,
-                                controls=[
-                                    ft.Text(
-                                        value="Indica las características que necesita tu contraseña. Puedes "
-                                              "generarla más de una vez si no estás conforme con el resultado:",
-                                        font_family="AlbertSansR",
-                                        size=16,
-                                        color=primaryTextColor
-                                    )
-                                ]
-                            ),
-                            ft.Row(
-                                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                                controls=[
-                                    ft.Column(
-                                        horizontal_alignment=ft.CrossAxisAlignment.END,
-                                        controls=[
-                                            self.switches[0],
-                                            self.switches[1]
-                                        ]
-                                    ),
-                                    ft.Column(
-                                        horizontal_alignment=ft.CrossAxisAlignment.END,
-                                        controls=[
-                                            self.switches[2],
-                                            self.switches[3]
-                                        ]
-                                    )
-                                ]
-                            ),
-                            ft.Row(
-                                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                                controls=[
-                                    ft.Text(value="Número máximo de caracteres:\n"
-                                                  "El número mínimo es de 7 caracteres y un máximo de 50.",
-                                            font_family="AlbertSansL",
-                                            color=primaryTextColor),
-                                    self.max_input
-                                ]
-                            ),
-                            ft.Row(
-                                controls=[
-                                    self.result, self.copybutton
-                                ]
-                            )
-                        ]
-                    )
-                )
-
-            case GenerateFormStyle.NUMBER:
-                self.submit_button.on_click = self.generate_cardnumber
-                self.bodycontent = ft.Container(
-                    width=524,
-                    height=150,
-                    content=ft.Column(
-                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-                        spacing=22,
-                        controls=[
-                            ft.Row(
-                              controls=[
-                                  ft.Text(value="Al darle al botón, se generará un número de tarjeta\n"
-                                                "válido para poder almacenarlo en la base de datos.",
-                                          font_family="AlbertSansL",
-                                          color=primaryTextColor)
-                              ]
-                            ),
-                            ft.Row(
-                                controls=[
-                                    self.result, self.copybutton
-                                ]
-                            )
-                        ]
-                    )
-                )
-
-            case _:
-                self.bodycontent = ft.Text("{nothing_to_show}")
+        # Form attributes
+        self.result = CustomTextField(read_only=True, expand=True)
 
         # Form settings
-        self.modal = True
+        self.submit_button = CustomElevatedButton(name="Generar", style=ButtonStyle.DEFAULT)
+        self.cancel_button.on_click = lambda _: self.page.close(self)
 
-        # F-Title
+        # Form title
         self.title = ft.Row(
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.Text(
-                    value=title,
-                    font_family="AlbertSansB",
-                    size=20
-                ),
-                ft.IconButton(
-                    ft.Icons.CLOSE_ROUNDED,
-                    icon_color=iconAccentGeneralFormColor,
-                    on_click=lambda _: self.page.close(self),
-                    highlight_color=selectedIconGeneralFormColor,
-                    hover_color=hoverIconGeneralFormColor
-                )
+                ft.Text(title, font_family="AlbertSansB", size=20, color=primaryTextColor), self.close_button
             ]
         )
 
-        # F-Content
-        self.content = self.bodycontent
+        self.actions = [self.cancel_button, self.submit_button]
 
-        # F-Buttons
-        self.actions = [self.submit_button]
+        self.__update_appearance()
 
-        # Form design
-        self.shape = ft.RoundedRectangleBorder(4)
-        self.bgcolor = bgGeneralFormColor
+    def __update_appearance(self):
+        match self.style:
+            case FormStyle.PASSWORD:
+                self.submit_button.on_click = self.__generate_password
+                self.number_input = CustomTextField(width=76, max_length=2, input_filter=ft.NumbersOnlyInputFilter())
+                self.fields = [
+                    CustomSwitch(title="Minúsculas", width=240, on_change=self.toggle_submit_button_state, value=True),
+                    CustomSwitch(title="Mayúsculas", width=240, on_change=self.toggle_submit_button_state),
+                    CustomSwitch(title="Números", width=240, on_change=self.toggle_submit_button_state),
+                    CustomSwitch(title="Caracteres especiales", width=240, on_change=self.toggle_submit_button_state),
+                ]
+
+                # Content
+                self.content.content = ft.Column(alignment=ft.MainAxisAlignment.SPACE_BETWEEN, spacing=22,
+                    controls=[
+                        ft.Row(wrap=True,
+                            controls=[
+                                ft.Text(
+                                    value="Indica las características que necesita tu contraseña. Puedes generarla "
+                                          "más de una vez si no estás conforme con el resultado:",
+                                    font_family="AlbertSansR", color=primaryTextColor, size=16
+                                )
+                            ]
+                        ),
+                        ft.Row(alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                                ft.Column(horizontal_alignment=ft.CrossAxisAlignment.END,
+                                    controls=[self.fields[0], self.fields[1]]
+                                ),
+                                ft.Column(horizontal_alignment=ft.CrossAxisAlignment.END,
+                                    controls=[self.fields[2], self.fields[3]]
+                                )
+                            ]
+                        ),
+                        ft.Row(alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                                ft.Text(
+                                    value="Número máximo de caracteres:",
+                                    font_family="AlbertSansB", color=primaryTextColor, spans=[
+                                        ft.TextSpan(text="\nEl número por defecto es de 12 caracteres y\npuedes "
+                                            "colocar un máximo de 99.", style=ft.TextStyle(font_family="AlbertSansL"))
+                                    ]
+                                ),
+                                self.number_input
+                            ]
+                        ),
+                        ft.Row(
+                            controls=[
+                                self.result,
+                                IconLink(ft.Icons.COPY_ROUNDED, style=IconLinkStyle.LIGHT,
+                                    function=self.copy_text, tooltip="copiar contraseña")
+                            ]
+                        )
+                    ]
+                )
+
+            case FormStyle.CC_NUMBER:
+                self.content = ft.Container(width=524, height=150)
+                self.submit_button.on_click = self.__generate_number
+
+                # Content
+                self.content.content = ft.Column(alignment=ft.MainAxisAlignment.SPACE_BETWEEN, spacing=22,
+                    controls=[
+                        ft.Row(wrap=True,
+                            controls=[
+                                ft.Text(
+                                    value="Al darle al botón, se generará un número de tarjeta válido para poder "
+                                          "almacenarlo en la base de datos.",
+                                    font_family="AlbertSansR", color=primaryTextColor, size=16
+                                )
+                            ]
+                        ),
+                        ft.Row(
+                            controls=[
+                                self.result,
+                                IconLink(ft.Icons.COPY_ROUNDED, style=IconLinkStyle.LIGHT,
+                                    function=self.copy_text, tooltip="copiar contraseña")
+                            ]
+                        )
+                    ]
+                )
+
+    def __generate_password(self, _: ft.ControlEvent) -> None:
+        password_length = 12
+        dictionary = self.__update_dictionary()
+        new_password = ""
+
+        if self.number_input.value:
+            try:
+                password_length = int(self.number_input.value)
+
+            except ValueError as text_number:
+                password_length = 12
+                logger.error(f"'{self.number_input.value}' no es un número válido.", text_number)
+
+        while len(new_password) < password_length:
+            new_password += random.choice(dictionary)
+
+        self.result.value = new_password
+        self.result.update()
+
+    def __update_dictionary(self) -> str:
+        dictionary = ""
+
+        if self.fields[0].get_value(): dictionary += ascii_lowercase
+        if self.fields[1].get_value(): dictionary += ascii_uppercase
+        if self.fields[2].get_value(): dictionary += digits
+        if self.fields[3].get_value(): dictionary += punctuation
+
+        return dictionary.replace(" ", "")
+
+    def __generate_number(self, _: ft.ControlEvent) -> None:
+        # Initialize faker class
+        faker = Faker('es_ES')
+
+        # Create new creditcard number
+        self.result.value = faker.credit_card_number()
+        self.result.update()
 
     def copy_text(self, cursor: ft.ControlEvent) -> None:
         self.page.set_clipboard(self.result.value)
-        cursor.control.badge = ft.Badge(
-            text="Copiado!",
-            bgcolor=ft.Colors.with_opacity(opacity=0.5, color=neutral80),
-            text_color=neutral05
-        )
-        cursor.control.update()
-        time.sleep(1)
-        cursor.control.badge.label_visible = False
-        cursor.control.update()
+        cursor.control.show_badge()
 
-    def toggle_generate_button_state(self, _: ft.ControlEvent) -> None:
-        if any((filter(lambda button: button.get_value(), self.switches))):
+    def toggle_submit_button_state(self, cursor: ft.ControlEvent) -> None:
+        if cursor and any((filter(lambda switch: switch.get_value(), self.fields))):  # Gets bool value from each switch
             self.submit_button.disabled = False
         else:
             self.submit_button.disabled = True
         self.submit_button.update()
-
-    def change_number_input(self, _: ft.ControlEvent) -> None:
-        if not self.max_input.value.isdigit():
-            self.max_input.value = ""
-            self.max_input.update()
-            return
-
-        num = int(self.max_input.value)
-        if num > 50:
-            self.max_input.value = "50"
-            self.max_input.update()
-
-    def generate_cardnumber(self, _: ft.ControlEvent) -> None:
-        # Initializes the class Faker
-        fake = Faker()
-
-        # Creates a new credit card number
-        new_creditcard_number = fake.credit_card_number()
-
-        # Updates value
-        self.result.value = new_creditcard_number
-        self.result.update()
-
-    def generate_password(self, _: ft.ControlEvent):
-        try:
-            if not self.max_input.value:
-                max_value = 7
-
-            else:
-                max_value = 7 if int(self.max_input.value) < 7 else int(self.max_input.value)
-
-        except ValueError as err:
-            print("OPS!", err)
-
-        else:
-            text_dict = ""
-
-            if self.switches[0].get_value():
-                text_dict += ascii_lowercase
-            if self.switches[1].get_value():
-                text_dict += ascii_uppercase
-            if self.switches[2].get_value():
-                text_dict += digits
-            if self.switches[3].get_value():
-                text_dict += punctuation
-
-            self.result.value = self.create_password(max_len=max_value, characters=text_dict)
-            self.result.update()
-
-    @staticmethod
-    def create_password(max_len: int, characters: str) -> str:
-        new_password = ""
-
-        while len(new_password) < max_len:
-            new_password += random.choice(characters)
-
-        return new_password
