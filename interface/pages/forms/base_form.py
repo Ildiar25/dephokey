914801ec879e0ea06667140c4ada2630fe6bb2f8
Joan <j.pastor1591@gmail.com>
2025-02-26 08:16:01 +0100

    Added new styles 'PASSWORD' & 'CC_NUMBER'

--- interface/pages/forms/base_form.py
+++ interface/pages/forms/base_form.py
@@ -10,6 +10,8 @@ class FormStyle(Enum):
     ADD = "add"
     EDIT = "edit"
     RESET = "reset"
+    PASSWORD = "password"
+    CC_NUMBER = "cc_number"
 
 
 class BaseForm(ft.AlertDialog):
@@ -17,21 +19,21 @@ class BaseForm(ft.AlertDialog):
         super().__init__()
 
         # General attributes
+        self.fields = []
         self.submit_button = CustomElevatedButton(name="Aceptar", style=ButtonStyle.DEFAULT, disabled=True)
         self.cancel_button = CustomElevatedButton(name="cancelar", style=ButtonStyle.CANCEL)
-        self.fields = []
-
-        # Form general settings
-        self.modal = True
-
-        # Form general content
-        self.actions = [self.cancel_button, self.submit_button]
-        self.content = ft.Container(width=550, height=378)
         self.span = ft.TextSpan(text="*", style=ft.TextStyle(font_family="AlbertSansB", color=dangerTextColor))
         self.close_button = ft.IconButton(
             ft.Icons.CLOSE_ROUNDED, icon_color=iconAccentGeneralFormColor, on_click=lambda _: self.page.close(self),
             highlight_color=selectedIconGeneralFormColor, hover_color=hoverIconGeneralFormColor
         )
+
+        # Form general settings
+        self.modal = True
+
+        # Form general content
+        self.content = ft.Container(width=550, height=378)
+        self.actions = [self.cancel_button, self.submit_button]
 
         # Form general design
         self.shape = ft.RoundedRectangleBorder(4)
