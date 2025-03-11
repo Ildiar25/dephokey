95ac7ad6ab9378e2d568fd89dd719a731cf22447
Joan <j.pastor1591@gmail.com>
2025-03-11 14:17:01 +0100

    Refactored how it creates and sends email

--- features/email_management/create_message.py
+++ features/email_management/create_message.py
@@ -1,35 +1,85 @@
+from email.mime.text import MIMEText
+from enum import Enum
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 from pathlib import Path
 from jinja2.exceptions import TemplateNotFound
 from jinja2 import Environment, FileSystemLoader
 
-from shared.utils.masker import mask_text
+from shared.utils.masker import mask_text, mask_email
 from shared.logger_setup import main_log as log
 
 
-class CreateMessage:
+class MessageStyle(Enum):
+    RESET = "reset"
+    QUERY = "querty"
 
-    def __init__(self, name: str, token: str) -> None:
+
+class CreateMessage(MIMEMultipart):
+    def __init__(self, style: MessageStyle, send_from: str | None = None, send_to: str | None = None,
+                 subject: str | None = None, token: str | None = None, name: str | None = None,
+                 content: str | None = None) -> None:
+        super().__init__()
+
+        # General attributes
+        self.path_img = Path(__file__).parent.joinpath("templates/assets/logotype.png")
+        self["From"] = send_from if send_from else "dephokey.team@gmail.com"
+        self["To"] = send_to if send_to else "dephokey.team@gmail.com"
+        self["Subject"] = subject if subject else "¡Tu token para Dephokey está listo!"
+
+        # Message attributes
+        self.style = style
         self.name = name
-        self.token: str = token
-        self.with_text: str = self.__plain_text()
-        self.with_html: str | None = self.__html_text()
+        self.token = token
+        self.img = self.__load_image()
+        self.with_text = content
+        self.with_html = None
 
-    def __plain_text(self) -> str:
-        return (f"Hola {self.name}!\nPor favor, introduce en el programa el código de siete caracteres "
-                f"proporcionado\npara poder actualizar tu contraseña:\n\n{self.token}\n\nSi no has realizado la "
-                f"petición, puedes ignorar este email.\n\nAtentamente,\nEl equipo Dephokey")
+        self.__update_appearance()
 
-    def __html_text(self) -> str | None:
+    def __update_appearance(self) -> None:
+        match self.style:
+            case MessageStyle.RESET:
+                self.with_text = (f"Hola {self.name}!\nPor favor, introduce en el programa el código de siete "
+                                  f"caracteres proporcionado\npara poder actualizar tu contraseña:\n\n{self.token}\n\n"
+                                  f"Si no has realizado la petición, puedes ignorar este email.\n\nAtentamente,\n"
+                                  f"El equipo Dephokey")
+                self.with_html = self.__load_template(
+                    template_name="reset_password.html", title=self.name, message=self.token
+                )
+
+            case MessageStyle.QUERY:
+                self.with_html = self.__load_template(
+                    template_name="user_request.html", title=self["subject"], message=self.with_text
+                )
+
+    def __load_image(self) -> MIMEBase | None:
+        try:
+            with open(self.path_img, "rb") as img:
+                logo = MIMEBase("logotype", "png")
+                logo.set_payload(img.read())
+                encoders.encode_base64(logo)
+                logo.add_header("Content-ID", "<logotype.png>")
+                logo.add_header("Content-Disposition", "inline", filename="logotype.png")
+                return logo
+
+        except PermissionError as not_allowed:
+            log.error(f"{type(not_allowed).__name__} | No se tienen permisos para leer el archivo: {not_allowed}.")
+            return None
+        except FileNotFoundError as not_found:
+            log.error(f"{type(not_found).__name__} | No se ha encontrado el logotipo para el email: {not_found}.")
+            return None
+
+    @staticmethod
+    def __load_template(template_name: str, title: str, message: str) -> str | None:
         # Loads file directory
         main_path = Path(__file__).parent
         env = Environment(loader=FileSystemLoader(f"{main_path}/templates"))
 
-        # Loads template
         try:
-            template = env.get_template("reset_password.html")
-            html_doc = template.render(name=self.name, code=self.token)
-
-            return html_doc
+            new_template = env.get_template(template_name)
+            return new_template.render(mail_title=title, mail_message=message)
 
         except TemplateNotFound as not_template:
             log.error(f"{type(not_template).__name__} | No se ha encontrado la plantilla HTML: {not_template}")
@@ -37,6 +87,17 @@ class CreateMessage:
             log.error(f"{type(unknown).__name__} | Un error inesperado ha ocurrido al procesar la platilla "
                       f"HTML: {unknown}")
 
+    def create(self) -> "CreateMessage":
+        if self.img:
+            self.attach(self.img)
+        if self.with_text:
+            text_part = MIMEText(self.with_text, "plain")
+            self.attach(text_part)
+        if self.with_html:
+            html_part = MIMEText(self.with_html, "html")
+            self.attach(html_part)
+        return self
+
     def __repr__(self) -> str:
-        return (f"<class CreateEmail(name={repr(self.name)}, code={repr(mask_text(self.token))}, "
-                f"with_text={repr(mask_text(self.with_text))}, with_html={repr(mask_text(self.with_html))})>")
+        return (f"<class CreateEmail(from={repr(mask_email(self['From']))}, to={repr(mask_email(self['To']))}, "
+                f"subject={repr(self['Subject'])}, content={repr(mask_text(self.with_text))})>")
--- features/email_management/send_email.py
+++ features/email_management/send_email.py
@@ -1,60 +1,28 @@
 import smtplib
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 from features.models.user import User
-from .create_message import CreateMessage
+from .create_message import CreateMessage, MessageStyle
 
 from shared.utils.masker import mask_number, mask_email
 from shared.logger_setup import main_log as log
 
 
 class SendEmail:
-    def __init__(self, user: User, token: str) -> None:
+    def __init__(self, msg_style: MessageStyle, **kwargs) -> None:
         # Server config
         self.smtp_host = "localhost"
         self.smtp_port = 1025
 
-        # User data
-        self.name = user.fullname.split(" ")[0]
-        self.recipient = user.email
+        self.message = CreateMessage(
+            style=msg_style,
+            **kwargs
+        ).create()
 
-        # Sender data
-        self.sender = "dephokey.team@gmail.com"
-
-        # Message
-        self.body = CreateMessage(self.name, token)
 
     def send(self) -> bool:
-        # New Message
-        msg = MIMEMultipart()
-        msg["From"] = self.sender
-        msg["To"] = self.recipient
-        msg["Subject"] = "Tu token para Dephokey está listo"
-
-        # Image attachment
-        try:
-            with open("features/email_management/templates/assets/logotype.png", "rb") as image:
-                img = MIMEBase("logotype", "png")
-                img.set_payload(image.read())
-                encoders.encode_base64(img)
-                img.add_header("Content-ID", "<logotype.png>")
-                img.add_header("Content-Disposition", "inline", filename="logotype.png")
-                msg.attach(img)
-
-        except FileNotFoundError as not_found:
-            log.error(f"{type(not_found).__name__} | No se ha encontrado el logotipo para el email: {not_found}.")
-        except PermissionError as not_allowed:
-            log.error(f"{type(not_allowed).__name__} | No se tienen permisos para leer el archivo: {not_allowed}.")
-
-        msg.attach(MIMEText(self.body.with_text, "plain"))
-        msg.attach(MIMEText(self.body.with_html, "html"))
-
         try:
             smtp = smtplib.SMTP(self.smtp_host, self.smtp_port)
-            smtp.send_message(msg)
+            smtp.send_message(self.message)
             smtp.quit()
             return True
 
@@ -70,6 +38,5 @@ class SendEmail:
             return False
 
     def __str__(self) -> str:
-        return (f"<class SendEmail(smtp_host={repr(mask_number(self.smtp_host))}, "
-                f"smtp_port={repr(mask_number(str(self.smtp_port)))}, sender={repr(mask_email(self.sender))}, "
-                f"recipient={repr(mask_email(self.recipient))}, body={self.body})>")
+        return (f"<class SendEmail(host={repr(self.smtp_host)}, port={repr(self.smtp_port)}, "
+                f"message={repr(self.message)})>")
