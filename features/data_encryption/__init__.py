b4e000839d5c79f91001ada1abf12becf9a912b1
Joan <j.pastor1591@gmail.com>
2025-03-19 17:33:13 +0100

    Refactor readability in data_encryption

--- features/data_encryption/__init__.py
+++ features/data_encryption/__init__.py
@@ -12,19 +12,26 @@ KEY_FILE = "key.key"
 
 def create_key() -> None:
     new_key = Fernet.generate_key()
+
     try:
         with open(KEY_PATH.joinpath(KEY_FILE).__str__(), "xb") as key_file:
             key_file.write(new_key)
         log.info(f"¡Archivo {repr(KEY_FILE)} creado!")
+
     except PermissionError as permission:
-        log.error(f"{type(permission).__name__} | "
-                  f"No se disponen de permisos para la creación del archivo {repr(KEY_FILE)}: {permission}")
+        log.error(
+            f"{type(permission).__name__} | "
+            f"No se disponen de permisos para la creación del archivo {repr(KEY_FILE)}: {permission}"
+        )
     except FileExistsError as already_exists:
-        log.error(f"{type(already_exists).__name__} | "
-                  f"El archivo {repr(KEY_FILE)} ya existe: {already_exists}")
+        log.error(
+            f"{type(already_exists).__name__} | El archivo {repr(KEY_FILE)} ya existe: {already_exists}"
+        )
     except Exception as unknown:
-        log.error(f"{type(unknown).__name__} | "
-                  f"Un error inesperado a ocurrido al tratar de crear el archivo {repr(KEY_FILE)}: {unknown}")
+        log.error(
+            f"{type(unknown).__name__} | "
+            f"Un error inesperado a ocurrido al tratar de crear el archivo {repr(KEY_FILE)}: {unknown}"
+        )
 
 
 # Check if key already exists:
--- features/data_encryption/core.py
+++ features/data_encryption/core.py
@@ -4,22 +4,24 @@ from cryptography.fernet import Fernet
 
 from shared.logger_setup import main_log as log
 
+# Create KEY path
+KEY_PATH = Path(__file__).parent
+
 
 def load_key() -> bytes | None:
+
+    # Prepare key for encrypt
     try:
-        # Create key path
-        key_path = Path(__file__).parent
-
-        with open(f"{key_path}/key.key", "rb") as key_file:
+        with open(f"{KEY_PATH}/key.key", "rb") as key_file:
             key = key_file.read()
-
         return key
 
     except FileNotFoundError as not_found:
         log.error(f"{type(not_found).__name__} | No se ha encontrado el archivo 'key': {not_found}")
     except Exception as unknown:
-        log.error(f"{type(unknown).__name__} | Un error inesperado a ocurrido al tratar de abrir el archivo "
-                     f"'key': {unknown}")
+        log.error(
+            f"{type(unknown).__name__} | Un error inesperado a ocurrido al tratar de abrir el archivo 'key': {unknown}"
+        )
 
 
 def encrypt_data(new_data: str) -> str:
@@ -32,15 +34,15 @@ def encrypt_data(new_data: str) -> str:
 
         # Encrypt data
         encrypted_data = fernet.encrypt(data_coded)
-
         return encrypted_data.decode()
 
     except TypeError as current_type:
         log.error(f"{type(current_type).__name__} | No se ha podido encriptar el archivo: {current_type}")
         return data_coded.decode()
     except Exception as unknown:
-        log.error(f"{type(unknown).__name__} | Un error inesperado ha ocurrido al tratar de encriptar el "
-                     f"archivo: {unknown}")
+        log.error(
+            f"{type(unknown).__name__} | Un error inesperado ha ocurrido al tratar de encriptar el archivo: {unknown}"
+        )
         return data_coded.decode()
 
 
@@ -54,13 +56,14 @@ def decrypt_data(load_data: str) -> str:
 
         # Decrypt data
         decrypted_data = fernet.decrypt(data_coded)
-
         return decrypted_data.decode(encoding="utf-8", errors="replace")
 
     except TypeError as current_type:
         log.error(f"{type(current_type).__name__} | No se ha podido desencriptar el archivo: {current_type}")
         return data_coded.decode()
     except Exception as unknown:
-        log.error(f"{type(unknown).__name__} | Un error inesperado ha ocurrido al tratar de desencriptar el "
-                     f"archivo: {unknown}")
+        log.error(
+            f"{type(unknown).__name__} | "
+            f"Un error inesperado ha ocurrido al tratar de desencriptar el archivo: {unknown}"
+        )
         return data_coded.decode()
